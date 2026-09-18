20f10ae4e7d4f5d370646dc9ae34c2fafb5ed0e3
coder123454321 <115930973+coder123454321@users.noreply.github.com>
2026-09-18 23:40:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstWord(s) {
   // your code here
+	 s = s.trim();
+
+    let index = s.indexOf(" ");
+
+    if (index === -1) {
+        return s;
+    }
+
+    return s.slice(0, index);
 }
 
 // Do not change the code below
